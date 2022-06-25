bb8d66ffd66c1aa311499a34f477d35536fac097
Zachinquarantine <zachinquarantine@yahoo.com>
2022-06-25 16:09:16 -0400

    Initial commit

---
+++ src/math/main.go
@@ -0,0 +1,27 @@
+package math
+
+import (
+	"fmt"
+)
+
+var SumofAccelerations int
+
+var KeplerianPeriod int
+
+var MassDensity int
+
+var OrbitalVelocity int
+
+var DragReference int
+
+var Mass int
+
+var DimensionlessDrag int
+
+func dividedragandmass() {
+	fmt.Println("TBD")
+}
+
+func Total() {
+	fmt.Println("0.5 * MassDensity * OrbitalVelocity * DimensionlessDrag")
+}
